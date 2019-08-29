8eb0f25426e3f0d68fc016d2f72641cb12f08cab
Erik Borella <erikborella@gmail.com>
2019-08-29 10:55:11 -0300

    database sql added

---
+++ database/sql.sql
@@ -0,0 +1,57 @@
+DROP DATABASE test;
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE IF NOT EXISTS `user` (
+	`registration` INTEGER AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `level` INTEGER NOT NULL,
+    UNIQUE(`email`),
+    PRIMARY KEY(`registration`)
+);
+
+CREATE TABLE IF NOT EXISTS `group` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `group_name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `participant` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_registration` INTEGER NOT NULL,
+    `group_id` INTEGER NOT NULL,
+    UNIQUE (`user_registration`),
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`user_registration`) REFERENCES `user`(`registration`),
+    FOREIGN KEY(`group_id`) REFERENCES `group`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `theme` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `theme` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `photo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `path` VARCHAR(255) NOT NULL,
+    `group_id` INTEGER NOT NULL,
+    `theme_id` INTEGER NOT NULL,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`group_id`) REFERENCES `group`(`id`),
+    FOREIGN KEY(`theme_id`) REFERENCES `theme`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `note` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `note` FLOAT NOT NULL,
+    `user_registration` INTEGER NOT NULL,
+    `photo_id` INTEGER NOT NULL,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`user_registration`) REFERENCES `user`(`registration`),
+    FOREIGN KEY(`photo_id`) REFERENCES `photo`(`id`)
+);
